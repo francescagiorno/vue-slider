39de430ae3b7053cef7bed8d9317f6f67b4a7e3c
francesca-giorno <francescagiorno1996@gmail.com>
2020-11-17 17:25:30 +0100

    parte dell'ex

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const slider = new Vue({
+  el:".big-container",
+  data:{
+    arr:["img/image1.jpg","img/image2.jpg","img/image3.jpg","img/image4.jpg"],
+    index: 0,
+  },
+
+  methods:{
+    prevSlider:function(){
+      if(this.index === 0){
+        this.index = this.arr.length
+      }else{
+        this.index--
+      }
+    },
+
+    nextSlider: function(){
+      if(this.index === this.arr.length){
+        this.index = 0
+      }else{
+      this.index++}
+    }
+  }
+})
